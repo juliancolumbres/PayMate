9d84c5f764161b33aa92048eea23c9de7377f080
Allen Tran <allentran@allens-macbook.local>
2021-12-20 13:11:31 -0800

    set up server

---
+++ app/server/server.js
@@ -0,0 +1,30 @@
+// const plaid = require("plaid");
+const bodyParser = require("body-parser");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV],
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+      "PLAID-SECRET": process.env.PLAID_SECRET,
+    },
+  },
+});
+
+const client = new PlaidApi(configuration);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "We got it!",
+  });
+});
+
+app.listen(8000, () => console.log("Listening on port 8000"));
