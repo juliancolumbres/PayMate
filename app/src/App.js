8fbd29e08c2d1426ee85cc7241ad99b0f7c66f4c
Allen Tran <allentran@allens-macbook.local>
2021-12-22 20:59:06 -0800

    added navbar with temp logo

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Mates from './pages/Mates';
 
 function App() {
   return (
-   <>
-   <h1>My App Content</h1>
-   </>
+    <>
+    <div className='header'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Dashboard} />
+          <Route path='/mates' component={Mates} />
+          <Route path='/family' component={Mates} />
+        </Routes>
+      </Router>
+    </div>
+    <div className='main'> 
+      <h3>My app content</h3>
+    </div>
+    </>
   );
 }
 
